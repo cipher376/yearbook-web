72852a070248b0b73c9a5f514d7fb34ac5dfb03b
Alfred Ntiamoah <ntiamoah376@yahoo.com>
2020-12-23 19:28:44 +0000

    Routes config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { RegisterComponent } from './public/register/register.component';
+import { LoginComponent } from './public/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'home',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  { path: '',   redirectTo: '/login', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component:  LoginComponent},  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
